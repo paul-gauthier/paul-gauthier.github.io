fd92d0ffae934e5818d57a77ffe0439bd36af3cf
Paul Gauthier <paul@aider.chat>
2026-04-08 14:15:59 -0700

    full build including Gemfile.lock

--- scripts/jekyll_build.sh
+++ scripts/jekyll_build.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
+set -euo pipefail
+
+rm Gemfile.lock
+
+touch Gemfile.lock
+
+# Build the Docker image
+docker build -t paulg-info-jekyll -f scripts/Dockerfile.jekyll .
+
+script_dir="$(cd "$(dirname "$0")" && pwd)"
+
+exec "$script_dir/jekyll_exec.sh" bundle lock --add-platform x86_64-linux
+exec "$script_dir/jekyll_exec.sh" bundle lock --add-platform aarch64-linux
+
 # Build the Docker image
 docker build -t paulg-info-jekyll -f scripts/Dockerfile.jekyll .
