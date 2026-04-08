110c1c139c700b5eb56551dd746066e5d5d30335
Paul Gauthier <paul@aider.chat>
2026-04-08 14:13:04 -0700

    chore: update Bundler version and split bundle lock platforms

--- scripts/bundle_lock.sh
+++ scripts/bundle_lock.sh
@@ -4,4 +4,5 @@ set -euo pipefail
 
 script_dir="$(cd "$(dirname "$0")" && pwd)"
 
-exec "$script_dir/jekyll_exec.sh" bundle lock --add-platform x86_64-linux --add-platform aarch64-linux
+exec "$script_dir/jekyll_exec.sh" bundle lock --add-platform x86_64-linux
+exec "$script_dir/jekyll_exec.sh" bundle lock --add-platform aarch64-linux
