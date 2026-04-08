afa9d8fd1678414a1c41fc0e005355006d43e0d2
Paul Gauthier <paul@aider.chat>
2026-04-08 14:16:11 -0700

    cleanup

--- scripts/bundle_lock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-script_dir="$(cd "$(dirname "$0")" && pwd)"
-
-exec "$script_dir/jekyll_exec.sh" bundle lock --add-platform x86_64-linux
-exec "$script_dir/jekyll_exec.sh" bundle lock --add-platform aarch64-linux
